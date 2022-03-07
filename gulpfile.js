d57520edbed9dce9ff48940b78764b38387c9c8f
Luis Saucedo <luigisaucedo.ls@gmail.com>
2022-03-07 06:43:58 -0400

    Instalación de: gulp-avif

--- gulpfile.js
+++ gulpfile.js
@@ -9,25 +9,13 @@ const plumber = require("gulp-plumber");
 const cache = require("gulp-cache");
 const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
+const avif = require("gulp-avif");
 
 function css( done ) {
     src("src/scss/**/*.scss") // 1. Identificar el archivo .SCSS a compilar
         .pipe( plumber() )
         .pipe( sass() ) // 2. Compilarlo
         .pipe( dest("build/css") ) // 3. Almacenarla en el disco duro   
-
-    done();
-}
-
-function versionWebp( done ) {
-
-    const opciones= {
-        quality: 50
-    };
-
-    src("src/img/**/*.{png,jpg}")
-        .pipe( webp(opciones) )
-        .pipe( dest("build/img") )
 
     done();
 }
@@ -45,6 +33,32 @@ function imagenes( done ) {
     done();
 }
 
+function versionWebp( done ) {
+
+    const opciones= {
+        quality: 50
+    };
+
+    src("src/img/**/*.{png,jpg}")
+        .pipe( webp(opciones) )
+        .pipe( dest("build/img") )
+
+    done();
+}
+
+function versionAvif( done ) {
+
+    const opciones= {
+        quality: 50
+    };
+
+    src("src/img/**/*.{png,jpg}")
+        .pipe( avif(opciones) )
+        .pipe( dest("build/img") )
+
+    done();
+}
+
 // Crear un watch
 function dev( done ) {
     watch("src/scss/**/*.scss", css)// parametros: 1.A qué archivo le haré watch y 2. Qué función estará asociada
@@ -54,4 +68,5 @@ function dev( done ) {
 exports.css = css;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.dev = parallel(imagenes, versionWebp, dev);
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
